b0f86f7f50fdc6b46d8156939557bb42108196b4
Rubén <Rubn@LAPTOP-HF6APOG6>
2020-02-04 19:01:20 +0100

    He añadido las dos interfaces, el constructor y los sets #4.close

--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -1,6 +1,24 @@
-public class Tarjeta{
+public class Tarjeta implements ITarjetaMonedero, ITarjetaDebito{
 	private String id;
 	private String dniTitular;
 	private int pin;
 	private float saldo;
+	
+	public Tarjeta(String id, String dniTitular, int pin, float saldo) {
+		setId(id);
+		setDniTitular(dniTitular);
+		setPin(pin);
+		setSaldo(saldo);
+
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+
+	}
+
+	public void setDniTitular(String dniTitular) {
+		this.dniTitular = dniTitular;
+
+	}
 }
