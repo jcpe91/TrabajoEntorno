d7464907d375a749413d4aca2be05048ab6aecbe
Rubén <Rubn@LAPTOP-HF6APOG6>
2020-02-04 19:30:07 +0100

    He añadido el constructo y el setSaldo closes #4

--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -3,4 +3,17 @@ public class Tarjeta implements ITarjetaMonedero, ITarjetaDebito {
 	private String dniTitular;
 	private int pin;
 	private float saldo;
+	
+	public Tarjeta(String id, String dniTitular, int pin, float saldo) {
+		setId(id);
+		setDniTitular(dniTitular);
+		setPin(pin);
+		setSaldo(saldo);
+
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+
+	}
 }
