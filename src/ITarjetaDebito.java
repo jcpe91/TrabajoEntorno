53cd388780661c1a197a121a3abb6a1dff724ff8
Jesús Miguel <Jess Miguel@DESKTOP-TSSMFRP>
2020-02-04 18:48:09 +0100

    Se implementan los metodos getDniTitular, getSaldo y setPin

--- src/ITarjetaDebito.java
+++ src/ITarjetaDebito.java
@@ -5,4 +5,11 @@ public interface ITarjetaDebito {
 	public void retirarCajero(float importe, int pin);
 	
 	public String getId();
+	
+	public String getDniTitular();
+
+	public int getSaldo();
+
+	public void setPin(int pin);
+
 }
