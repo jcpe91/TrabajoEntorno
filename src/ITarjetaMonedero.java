53abce751bf7da8d1253ecb5d5e355ffa6ceefa0
Mario Lozano <lozanogallardomario@gmail.com>
2020-02-04 19:20:53 +0100

    He corregido algunos errores de la interfaz

--- src/ITarjetaMonedero.java
+++ src/ITarjetaMonedero.java
@@ -1,4 +1,13 @@
 
 public interface ITarjetaMonedero {
+	public void comprar(float importe, String dni);
+
+	public String getId();
+
+	public String getDniTitular();
+
+	public int getSaldo();
+
+	public void setPin(int num);
 
 }
