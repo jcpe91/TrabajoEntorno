445e1ecf0ee8fb7cc0b5e7b7fc49a8048cc2355e
Mario Lozano <lozanogallardomario@gmail.com>
2020-02-05 10:29:21 +0100

    He creado la clase principal y dos objetos de tipo iTarjetaMonedero e ITarjetaDebito usando los constructores

---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		ITarjetaMonedero monedero = new Tarjeta("13247453753", "29553065C", 1594, 200);
+		ITarjetaDebito tarjeta = new Tarjeta("1324567985", "29556545T", 1326, 200000);
+	}
+}
